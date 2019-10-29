7deda9ac6f983e6e6f081512997590748f90b41e
ivan-gorlanov-yl <56786107+ivan-gorlanov-yl@users.noreply.github.com>
2019-10-29 14:37:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import sys
 import MainMenu
 
 app = Main_menu.QApplication(sys.argv)
-main_window = Main_menu.Main_menu()
+main_window = MainMenu.MainMenu()
 main_window.show()
 sys.exit(app.exec())
